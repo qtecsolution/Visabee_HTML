b00f8e69fdafefc1f6b03eb0a06d0721f6435be8
superSimpleJahir <supersimple.jahir@gmail.com>
2024-04-01 15:46:59 +0600

    [Done] header section

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+const navbar = document.querySelector('.navbar');
+const navBtn = document.querySelector('#navbarNavBtn');
+const navNav = document.querySelector('#navbarNav');
+const navLinks = document.getElementsByClassName('nav-link');
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener('click', function () {
+    if (this.classList.contains('dropdown-toggle')) {
+      return;
+    }
+
+    let current = document.getElementsByClassName('active');
+    current[0].className = current[0].className.replace(' active', '');
+
+    this.className += ' active';
+    navNav.classList.toggle('open');
+  });
+}
+
+//navbar show and hide functionality on mobile
+navBtn.addEventListener('click', () => {
+  navNav.classList.toggle('open');
+});
