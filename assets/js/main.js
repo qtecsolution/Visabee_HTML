b5012fdc2e5f564aad9aadc0f8f51240fe4ffd84
superSimpleJahir <supersimple.jahir@gmail.com>
2024-04-08 16:24:07 +0600

    [Done] media swiper-slide add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,30 @@
+var swiper1 = new Swiper(".mySwiper1", {
+  slidesPerView: 1,
+  navigation: {
+    nextEl: "#button-next-relacionados",
+    prevEl: "#button-prev-relacionados",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+});
+
 // nice-select2
 document.addEventListener("DOMContentLoaded", function (e) {
   var els = document.querySelectorAll(".selectize");
   els.forEach(function (select) {
     NiceSelect.bind(select);
-    console.log(select)
+    console.log(select);
   });
 });
